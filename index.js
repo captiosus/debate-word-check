f462a7aa1d026ee139ee5ac09c58ce4cfb8a98ca
GuyWithTheGlasses <kevin.a.yan@gmail.com>
2016-03-29 12:31:07 -0400

    Pie charts are hard

--- index.js
+++ index.js
@@ -14,35 +14,39 @@ var color = function(i){
 data = [{"label":"Bernie", "value":30}, 
         {"label":"Hillary", "value":70}];
 
-var savage = d3.select("body")
+var piechart = function(cx,cy){
+    var savage = d3.select("body")
         .append("svg:svg")              
         .data([data])                   
-            .attr("width", w)           
-            .attr("height", h)
+          .attr("width", w)           
+          .attr("height", h)
         .append("svg:g")                
-            .attr("transform", "translate(" + r + "," + r + ")");   
+          .attr("transform", "translate(" + cx + "," + cy + ")");   
 
-var arc = d3.svg.arc()              
-    .outerRadius(r);
+    var arc = d3.svg.arc()              
+	.outerRadius(r);
 
-var pie = d3.layout.pie()          
-    .value(function(d) { return d.value; });    
+    var pie = d3.layout.pie()          
+	.value(function(d) { return d.value; });    
 
-var arcs = savage.selectAll("g.slice")    
-    .data(pie)                          
-    .enter()                            
-      .append("svg:g")                
-        .attr("class", "slice");    
+    var arcs = savage.selectAll("g.slice")    
+	.data(pie)                          
+	.enter()                            
+	  .append("svg:g")                
+            .attr("class", "slice");    
 
     arcs.append("svg:path")
-      .attr("fill", function(d, i) { return color(i); } ) 
-      .attr("d", arc);    
-                                
+	.attr("fill", function(d, i) { return color(i); } ) 
+	.attr("d", arc);    
+    
     arcs.append("svg:text")                                     
-      .attr("transform", function(d) {                    
-          d.innerRadius = 0;
-          d.outerRadius = r;
-          return "translate(" + arc.centroid(d) + ")";        
-      })
-      .attr("text-anchor", "middle")                          
-      .text(function(d, i) { return data[i].label; });        
+	.attr("transform", function(d) {                    
+            d.innerRadius = 0;
+            d.outerRadius = r;
+            return "translate(" + arc.centroid(d) + ")";        
+	})
+	.attr("text-anchor", "middle")                          
+	.text(function(d, i) { return data[i].label; });   
+};     
+
+piechart(250,250);
